400a4b9c61e7232302bd718fd5fe1d60b2695542
Inna-Demchuk <idem4uck@gmail.com>
2022-07-21 00:51:17 +0300

    02-lightbox +

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__card"><a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`;
+    }).join('');
+};
+
+galleryList.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+const gallery = new SimpleLightbox('.gallery__item', { captionSelector: 'img', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
