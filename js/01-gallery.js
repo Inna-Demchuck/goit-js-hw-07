3201497b2cc07b24101ba64e5470036913ce735a
Inna-Demchuk <idem4uck@gmail.com>
2022-07-20 23:11:31 +0300

    01-gallery +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item"><a class="gallery__link href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
+    }).join('');
+};
+
+galleryList.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+galleryList.addEventListener('click', onClickGallery);
+
+function onClickGallery(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`<img src = "${e.target.dataset.source}">`);
+
+    instance.show(() => window.addEventListener('keydown', onKeyPress));
+
+
+    function onKeyPress(e) {
+        if (e.key === 'Escape') {
+            instance.close(() => window.removeEventListener('keydown', onKeyPress));
+            return;
+        }
+
+    };
+};
